4cee423f50c374d0958fc35e39027fde5ffe74bd
Bharathsai-07 <bharathsainerella07@gmail.com>
2026-01-14 16:05:49 +0530

    added lines on discover and connections page

--- socialmedia/src/components/PostCard.jsx
+++ socialmedia/src/components/PostCard.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 import moment from 'moment'
 import { useState } from 'react'
 import { dummyUserData } from '../assets/assets'
-
-
+import{useNavigate} from 'react-router-dom'
 
 const PostCard = ({ post }) => {
 
@@ -19,10 +18,13 @@ const PostCard = ({ post }) => {
             setLikes([...likes,currentUser.id])
         }
     }
+
+    const navigate=useNavigate()
+
   return (
     <div className='bg-white rounded-xl shadow p-4 space-y-4 max-w-2xl'>
         {/* user info */}
-        <div className='inline-flex items-center gap-3 cursor-pointer'>
+        <div onClick={()=>navigate('/profile/ + post.user._id')} className='inline-flex items-center gap-3 cursor-pointer'>
             <img src={post.user.profile_picture} alt="" className='w-10 h-10 rounded-full shadow'/>
             <div>
                 <div className='flex items-center space-x-1'>
