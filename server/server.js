00588e9f0363729d59b327071da29ccacf17cbbe
Bharathsai-07 <bharathsainerella07@gmail.com>
2026-01-16 19:26:36 +0530

    deployment changes

--- server/server.js
+++ server/server.js
@@ -8,18 +8,22 @@ import { serve } from "inngest/express";
 
 const app= express();
 
-await connectDB();
-
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*',
+}));
+let isConnected = false;
 
-app.get('/', (req,res)=>res.send('Server is Running'))
-app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use(async (req, res, next) => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  next();
+});
 
-const PORT = process.env.PORT || 4000;
+app.get('/', (req, res) => res.send('Server is Running'));
 
- 
-
-app.listen(PORT , ()=>console.log(`server is running on port ${PORT}`))
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
 export default app;
